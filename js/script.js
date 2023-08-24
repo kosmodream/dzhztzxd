b910e8677185408d837eaf877993cf56bbeeaba3
StillMix <stillmixplaygames@gmail.com>
2023-08-24 18:05:35 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,58 +1,45 @@
-//Проще было начать с сааааааааааааааааамого начала, мы js проходили так, что даже проходили не назвать, а пользовались только для курсовой и то, всё с инета брали
-let minus = document.querySelector('.minus');
-let plus = document.querySelector('.plus');
-let del = document.querySelector('.del');
-let ymn = document.querySelector('.ymn');
-let input = document.querySelector('.vvod');
-let ravno = document.querySelector('.ravno');
-let popup = document.querySelector('.popup');
-let btns = document.querySelectorAll('.btn');
-let yesBtn = document.querySelector('.da');
-let noBtn = document.querySelector('.net');
-let clearBtn = document.querySelector('.clear');
-
-function ch(e){
-    input.value = input.value + e.textContent
-}
-
-ravno.addEventListener('click', () => {
-    try {
+document.addEventListener("DOMContentLoaded", function () {
+    // Получаем ссылки на элементы
+    const input = document.querySelector(".vvod");
+    const buttons = document.querySelectorAll(".btn");
+    const clearButton = document.querySelector(".clear");
+    const equalsButton = document.querySelector(".ravno");
+    const popup = document.querySelector(".popup");
+    const popupText = popup.querySelector('a');
+    let yesBtn = document.querySelector('.da');
+    let noBtn = document.querySelector('.net');
+    let btnCH ;
+    // Добавляем обработчик клика для всех кнопок
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        popupText.textContent = `Хотите ли вы добавить цифру ${button.textContent}?`
+        popup.classList.add('active')
+        btnCH = button.textContent
+      });
+    });
+  
+    // Обработчик клика для кнопки "Очистить строку"
+    clearButton.addEventListener("click", function () {
+      input.value = "";
+    });
+  
+    // Обработчик клика для кнопки "="
+    equalsButton.addEventListener("click", function () {
+      try {
+        // Вычисляем результат и выводим его в поле ввода
         input.value = eval(input.value);
-    } catch (error) {
-        input.value = 'Ошибка';
-    }
-})
-//получилось только изменил грубо говоря так 
-function otkr(e) {
-    const buttonText = e.target.textContent;
-    numberToAdd = buttonText;
-    
-    const popupText = popup.textContent + `${buttonText}?`;
-    const popupContent = popup.querySelector('a');
-    popupContent.textContent = (popupText);
-   
-    if (!popup.classList.contains('active')) {
-        popup.classList.add('active');
-    }
-    
-    yesBtn.addEventListener('click', addNumberAndClose);
-    noBtn.addEventListener('click', zakr);
-}
+      } catch (error) {
+        input.value = "Ошибка";
+      }
+    });
+    yesBtn.addEventListener('click', () => {
+        input.value += btnCH;
+        popup.classList.remove('active')
+    })
+    noBtn.addEventListener('click', () => {
+        popup.classList.remove('active')
+    })
+  });
+  
 
-//слишком нагружено проще передавать само название кнопки numberToAdd убери и подумай как
-function addNumberAndClose() {
-    if (numberToAdd !== "") {
-        input.value += numberToAdd;
-        numberToAdd = ""; 
-        popup.classList.remove('active');
-    }
-    yesBtn.removeEventListener('click', addNumberAndClose);
-}
-//передавай название кнопки
-btns.forEach(btn => {
-    btn.addEventListener('click', otkr);
-});
-
-clearBtn.addEventListener('click', () => {
-    input.value = "";
-});
+  
